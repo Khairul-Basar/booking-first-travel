384dc2ee6b8ac5e49f9f40bc15bdc7b5145f7756
Khairul-Basar <khairulbasar130@gmail.com>
2023-12-27 23:25:20 +0600

    hotel crud operation have done

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -13,4 +13,51 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Update Hotel
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    console.log(updatedHotel);
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Single Hotel
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const hotel = await Hotel.findById(id);
+    console.log(hotel);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get All Hotel
+router.get("/", async (req, res) => {
+  try {
+    const allHotels = await Hotel.find();
+    res.status(200).json(allHotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete Hotel
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel Has been Deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
