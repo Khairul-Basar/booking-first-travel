b178c8a75c80e1bda0826109cf0f6185b34e99ca
Khairul-Basar <khairulbasar130@gmail.com>
2024-02-03 16:37:09 +0600

    implemented verify authentication and verify user in verifyToken file

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const User = require("../models/userSchema");
+const createError = require("../utils/error");
 const router = express.Router();
+const {
+  updateUser,
+  singleUser,
+  allUsers,
+  deleteUser,
+} = require("../controllers/user");
+const { verifyToken, verifyUser } = require("../utils/verifyToken");
 
-router.get("/", (req, res) => {
-  res.send("users endpoint");
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("Hello User, You are logged in.");
 });
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("Hello User, You are logged in and you can delete your account.");
+});
+// Update User
+router.put("/:id", verifyToken, updateUser);
+
+// Single User
+router.get("/:id", singleUser);
+
+// Get All User
+router.get("/", allUsers);
+
+// Delete User
+router.delete("/:id", deleteUser);
 
 module.exports = router;
---
+++ server/utils/verifyToken.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const createError = require("./error");
+const User = require("../models/userSchema");
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.Authorization;
+  if (!token) return next(createError(401, "You are not Authenticated!"));
+
+  const decode = jwt.verify(token, process.env.SECRET);
+  if (Date.now() > decode.exp)
+    return next(createError(401, "Your token isn't valid!"));
+  const user = await User.findById(decode.id);
+  if (!user) return next(createError(401, "You are not Authenticated!"));
+
+  req.user = user;
+
+  next();
+};
+
+const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You aren't Authenticated!"));
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyUser,
+};
