829ec868df63578a69683c7f4e37f2a550a79576
Khairul-Basar <khairulbasar130@gmail.com>
2024-02-18 22:48:53 +0600

    Featured component are fetched data

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import "./featured.css";
+import useFetch from "../../hooks/useFetch";
 
 function Featured() {
+  const { data, loading, error } = useFetch(
+    "http://localhost:8800/api/hotels/countbycity?cities=berlin,Dubai,Lisbone"
+  );
   return (
     <div className="propertyContainer">
-      <div className="propertyItem">
-        <img
-          src="https://i.ibb.co/m0FKTZD/aripata.webp"
-          alt=""
-          className="propertyImg"
-        />
-        <div className="propertyTitles">
-          <h1>Dublin</h1>
-          <h2>123 Properties</h2>
-        </div>
-      </div>
-      <div className="propertyItem">
-        <img
-          src="https://i.ibb.co/X8ZNhWJ/690334.webp"
-          alt=""
-          className="propertyImg"
-        />
-        <div className="propertyTitles">
-          <h1>Austin</h1>
-          <h2>532 Properties</h2>
-        </div>
-      </div>
-      <div className="propertyItem">
-        <img
-          src="https://i.ibb.co/hFM5W2W/689422.webp"
-          alt=""
-          className="propertyImg"
-        />
-        <div className="propertyTitles">
-          <h1>Reno</h1>
-          <h2>533 Properties</h2>
-        </div>
-      </div>
+      {loading ? (
+        "Loading...."
+      ) : (
+        <>
+          <div className="propertyItem">
+            <img
+              src="https://i.ibb.co/m0FKTZD/aripata.webp"
+              alt=""
+              className="propertyImg"
+            />
+            <div className="propertyTitles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} Properties</h2>
+            </div>
+          </div>
+          <div className="propertyItem">
+            <img
+              src="https://i.ibb.co/X8ZNhWJ/690334.webp"
+              alt=""
+              className="propertyImg"
+            />
+            <div className="propertyTitles">
+              <h1>Dubai</h1>
+              <h2>{data[1]} Properties</h2>
+            </div>
+          </div>
+          <div className="propertyItem">
+            <img
+              src="https://i.ibb.co/hFM5W2W/689422.webp"
+              alt=""
+              className="propertyImg"
+            />
+            <div className="propertyTitles">
+              <h1>Lisbone</h1>
+              <h2>{data[2]} Properties</h2>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,10 +1,10 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
