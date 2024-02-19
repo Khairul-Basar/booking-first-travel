5815dfdfae5f31cedcd6d1d73f6dc5522b78cdf5
Khairul-Basar <khairulbasar130@gmail.com>
2024-02-19 21:04:36 +0600

    all hotels controller are modified for search query and min max prices

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -42,7 +42,6 @@ const allHotels = async (req, res, next) => {
   const { min, max, ...others } = req.query;
   try {
     const hotels = await Hotel.find({
-      ...others,
       cheapestPrice: { $gt: min | 1, $lt: max || 999 },
     }).limit(req.query.limit);
 
