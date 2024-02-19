43481426f1d9ec84c8b6510384c3a2b7f2744df9
Khairul-Basar <khairulbasar130@gmail.com>
2024-02-19 16:39:05 +0600

    Fetched Data in Property List component

--- client/src/components/property/PropertyList.jsx
+++ client/src/components/property/PropertyList.jsx
@@ -1,65 +1,39 @@
 import React from "react";
 import "./propertyList.css";
+import useFetch from "../../hooks/useFetch";
 
 function PropertyList() {
+  const { data, loading, error } = useFetch(
+    "http://localhost:8800/api/hotels/countbytype"
+  );
+
+  const images = [
+    "https://i.ibb.co/z2r923K/477697253.jpg",
+    "https://i.ibb.co/cLgg8SL/6e8294d75f648eab2cd2818f0a40854367e584a5.jpg",
+    "https://i.ibb.co/6WW87CS/477697253.jpg",
+    "https://i.ibb.co/ySxD85L/506591607.jpg",
+    "https://i.ibb.co/rFqQkpz/240848933.jpg",
+  ];
   return (
     <div className="pListContainer">
-      <div className="pListItem">
-        <img
-          src="https://i.ibb.co/z2r923K/477697253.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Hotels</h1>
-          <h2>233 hotels</h2>
-        </div>
-      </div>
-
-      <div className="pListItem">
-        <img
-          src="https://i.ibb.co/cLgg8SL/6e8294d75f648eab2cd2818f0a40854367e584a5.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Apartments</h1>
-          <h2>200 apartments</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://i.ibb.co/6WW87CS/477697253.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Resorts</h1>
-          <h2>100 Resorts</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://i.ibb.co/ySxD85L/506591607.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Villas</h1>
-          <h2>290 villas</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://i.ibb.co/rFqQkpz/240848933.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Cabins</h1>
-          <h2>532 cabins</h2>
-        </div>
-      </div>
+      {loading ? (
+        "Property Type Loading..."
+      ) : (
+        <>
+          {data &&
+            images.map((image, i) => (
+              <div className="pListItem" key={i}>
+                <img src={image} alt="" className="pListImg" />
+                <div className="pListTitle">
+                  <h1>{data[i]?.type}</h1>
+                  <h2>
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
   );
 }
