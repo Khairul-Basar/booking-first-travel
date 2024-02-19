f8b1d97a88e3d133acf60f1a8bf0eb949f0fa354
Khairul-Basar <khairulbasar130@gmail.com>
2024-02-19 18:29:27 +0600

    something modified in client

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -39,8 +39,13 @@ const singleHotel = async (req, res) => {
 
 // All Hotel
 const allHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
+
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
