6ac75bdfd3287778b14c46ff6f85f56f6fe3241e
Khairul-Basar <khairulbasar130@gmail.com>
2024-02-19 12:03:40 +0600

    implemented countByType and getHotelRooms route and controller

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -1,4 +1,5 @@
 const Hotel = require("../models/hotelSchema");
+const Room = require("../models/roomeSchema");
 
 // Create Hotel
 const createHotel = async (req, res) => {
@@ -72,6 +73,40 @@ const countByCity = async (req, res, next) => {
   }
 };
 
+const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getHotelRooms = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    const list = await Promise.all(
+      hotel.rooms.map((room) => {
+        return Room.findById(room);
+      })
+    );
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   createHotel,
   updateHotel,
@@ -79,4 +114,6 @@ module.exports = {
   allHotels,
   deleteHotel,
   countByCity,
+  countByType,
+  getHotelRooms,
 };
